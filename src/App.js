bc61056628a66cff20cb6d9e3f86f5b66c1f5c78
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-04 10:36:34 -0600

    button click rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Home } from './Home';
+
 
 function App() {
+  const user = {id: 1, name: "Leo" }
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Home user={user} />
     </div>
   );
 }
